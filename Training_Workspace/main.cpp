1cd06ef594b9fa430aad4d01924d9ff5303ef540
hauct131 <chiemtrunghau131@gmail.com>
2026-01-20 15:47:48 +0700

    Init CP

---
+++ Training_Workspace/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define int ll
+
+void solve(){
+    int n; cin >> n;
+    cout << n << endl;
+    return;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    if(fopen("input.txt", "r")){
+        freopen("input.txt", "r", stdin); // Đọc từ input
+        freopen("output.txt", "w", stdout); // Ghi ra output
+    }
+    solve();
+    return 0;
+}
