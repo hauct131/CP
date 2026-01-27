02bba13a4814970fd9daf0b85915220b6bf80848
hauct131 <chiemtrunghau131@gmail.com>
2026-01-27 23:58:18 +0700

    Update solutions 2026-01-27 23:58:18

--- Khac/monsterhunter/monsterhunter_Full.cpp
+++ Khac/monsterhunter/monsterhunter_Full.cpp
@@ -19,9 +19,34 @@ using namespace std;
 #define int ll
 
 #define endl "\n"
+const int INF = 1e18;
 
+int n, p, h;
+string s;
+int res = INF;
+
+void Try(int idx, int curP, int curH, int cnt){
+    if(cnt >= res) return;
+
+    if(idx > n){
+        res = min(res, cnt);
+        return;
+    }
+
+    if(s[idx] == '#') Try(idx + 1, curP, curH, cnt);
+    else if(s[idx] == 'P') Try(idx + 1, curP + 1, curH, cnt);
+    else if(s[idx] == 'H') Try(idx + 1, curP, curH + 1, cnt);
+    else if(s[idx] == 'M'){
+        if(curP - 1 > 0) Try(idx + 1, curP - 1, curH, cnt + 1);
+        if(curH - 1 > 0) Try(idx + 1, curP, curH - 1, cnt);
+    }
+}
 void solve(){
-
+    cin >> n >> p >> h; cin.ignore();
+    cin >> s;
+    s = '#' + s;
+    Try(1, p, h, 0);
+    cout << (res == INF ? -1 : res);
 }
 
 signed main()
