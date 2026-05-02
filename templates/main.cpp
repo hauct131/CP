353b5ccd83ede78949663f6508f15231bfcd81f6
hauct131 <chiemtrunghau131@gmail.com>
2026-05-02 16:45:06 +0700

    Update template

--- templates/main.cpp
+++ templates/main.cpp
@@ -1,25 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Để chuyển giữa đọc file và nhập từ bàn phím, chỉ cần comment/uncomment dòng sau:
-#define FILE_IO
-
-void solve() {
-    // CODE CỦA BẠN Ở ĐÂY
-    int a, b;
-    cin >> a >> b;
-    cout << a + b << '\n';
-}
-
-int main() {
+void init() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    #ifdef FILE_IO
+    #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
+}
 
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+template <class T> void print_v(vector<T> &v) {
+    cout << "{";
+    for (auto x : v) cout << x << ",";
+    cout << "\b}\n";
+}
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+void solve() {
+    cout << "Helo";
+}
+
+int main() {
+    init();
     solve();
     return 0;
 }
+
