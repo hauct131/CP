2b756a1d72192b55bbed0ebd7b94b9dd61d4c0b7
hauct131 <chiemtrunghau131@gmail.com>
2026-05-02 16:45:31 +0700

    Init problem

---
+++ solutions/LQDOJ/monsterhunter/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void init() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+template <class T> void print_v(vector<T> &v) {
+    cout << "{";
+    for (auto x : v) cout << x << ",";
+    cout << "\b}\n";
+}
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+void solve() {
+    cout << "Helo";
+}
+
+int main() {
+    init();
+    solve();
+    return 0;
+}
