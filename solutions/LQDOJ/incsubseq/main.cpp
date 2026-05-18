b5d7c16aca0e9f6b0426839c7af097460e2c8222
hauct131 <chiemtrunghau131@gmail.com>
2026-05-18 22:57:12 +0700

    Init incsubseq

---
+++ solutions/LQDOJ/incsubseq/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define int ll
+
+#define endl "\n"
+
+void init() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void solve(){
+
+}
+
+signed main()
+{
+    init();
+
+    solve();
+
+    return 0;
+}
